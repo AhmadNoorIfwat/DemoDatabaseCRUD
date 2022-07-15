7ffcff4c0b6e827d86faf5ca3760989844383cad
AhmadNoorIfwat <21010771@myrp.edu.sg>
2022-07-15 16:04:30 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/c346/id21010771/demodatabasecrud/Note.java
@@ -0,0 +1,28 @@
+package sg.edu.c346.id21010771.demodatabasecrud;
+
+import java.io.Serializable;
+
+public class Note implements Serializable {
+    private 	int id;
+    private 	String noteContent;
+
+    public Note( int id, String noteContent  ) {
+        this.id = id;
+        this.noteContent = noteContent;
+    }
+
+    public int getId() {  return id;  }
+
+    public String getNoteContent() { return noteContent; }
+
+    public void setNoteContent(String noteContent) {
+        this.noteContent = noteContent;
+    }
+
+    @Override
+    public String toString() { return "ID:" + id + ", " + noteContent;  }
+
+}
+
+
+
